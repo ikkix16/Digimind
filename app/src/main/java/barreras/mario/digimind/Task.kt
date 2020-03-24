cd7526e967055e644aeef2bcd8c37e6d7396fe3a
IkkiX <marioxbarreras@gmail.com>
2020-03-23 20:29:33 -0700

    Initial commit

---
+++ app/src/main/java/barreras/mario/digimind/Task.kt
@@ -0,0 +1,3 @@
+package barreras.mario.digimind
+
+data class Task (var title:String, var days:ArrayList<String>, var time:String)
---
+++ app/src/main/java/barreras/mario/digimind/ui/notification/NotificationsFragment.kt
@@ -0,0 +1,31 @@
+package barreras.mario.digimind.ui.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import barreras.mario.digimind.R
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_navigation_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
